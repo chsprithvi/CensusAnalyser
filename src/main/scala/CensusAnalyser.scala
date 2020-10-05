80a0273287d87a8c825bd88502afa5af48797c96
chsprithvi <chsprithvi@gmail.com>
2020-10-05 12:56:07 +0530

    [Siddhartha]Added:Function to load state data file

--- src/main/scala/CensusAnalyser.scala
+++ src/main/scala/CensusAnalyser.scala
@@ -30,6 +30,33 @@ class CensusAnalyser {
       case _: FileNotFoundException => throw new CensusAnalyserException(CustomException.wrongFilePath)
     }
   }
+  def loadIndiaStateCode(filePath:String):Int={
+    try {
+      if(!filePath.endsWith(".csv")){
+        throw new CensusAnalyserException(CustomException.wrongFileType)
+      }
+      val fileReader = Source.fromFile(filePath)
+      var countRow = 0
+      for (line <- fileReader.getLines()) {
+        val cols = line.split(",").map(_.trim)
+
+        if (cols.length != 4){
+          throw new CensusAnalyserException(CustomException.wrongDelimiter)
+        }
+
+        if (countRow == 0){
+          if(cols(0) != "SrNo" || cols(1) != "State Name" || cols(2) != "TIN" || cols(3) != "StateCode"){
+            throw new CensusAnalyserException(CustomException.wrongHeaders)
+          }
+        }
+        countRow += 1
+      }
+      countRow - 1
+    }
+    catch {
+      case _: FileNotFoundException => throw new CensusAnalyserException(CustomException.wrongFilePath)
+    }
+  }
 }
 object CensusAnalyser{
   def main(args: Array[String]): Unit = {

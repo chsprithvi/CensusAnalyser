aed0868d052c2e304fbd0aee238deae44c9dba72
chsprithvi <chsprithvi@gmail.com>
2020-10-05 11:58:30 +0530

    [Siddhartha]Added:to load the csv file

---
+++ src/main/scala/CensusAnalyser.scala
@@ -0,0 +1,41 @@
+import java.io.FileNotFoundException
+
+import scala.io.Source
+
+class CensusAnalyser {
+  def loadIndiaCensusData(filePath: String): Int = {
+    try {
+      if(!filePath.endsWith(".csv")){
+        throw new CensusAnalyserException(CustomException.wrongFileType)
+      }
+      val fileReader = Source.fromFile(filePath)
+      var countRow = 0
+      for (line <- fileReader.getLines()) {
+        val cols = line.split(",").map(_.trim)
+
+        if (cols.length != 4){
+          throw new CensusAnalyserException(CustomException.wrongDelimiter)
+        }
+
+        if (countRow == 0){
+          if(cols(0) != "State" || cols(1) != "Population" || cols(2) != "AreaInSqKm" || cols(3) != "DensityPerSqKm"){
+            throw new CensusAnalyserException(CustomException.wrongHeaders)
+          }
+        }
+        countRow += 1
+      }
+      countRow - 1
+    }
+    catch {
+      case _: FileNotFoundException => throw new CensusAnalyserException(CustomException.wrongFilePath)
+    }
+  }
+}
+object CensusAnalyser{
+  def main(args: Array[String]): Unit = {
+    val INDIA_CENSUS_CSV_FILE_PATH   = "./src/test/resources/IndiaStateCensusData.csv"
+    val censusAnalyzer=new CensusAnalyser()
+    val numOfLines=censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH)
+    println(numOfLines)
+  }
+}
